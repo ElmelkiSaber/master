34e9063ac5bfea4d561d43e3d8bc71a9c555e557
Elmelkisaber <saber.amdoun@gmail.com>
2019-02-23 13:45:19 +0100

    fix fos + load fixtures

--- src/UserBundle/Entity/User.php
+++ src/UserBundle/Entity/User.php
@@ -1,34 +1,28 @@
 <?php
+// src/AppBundle/Entity/User.php
 
 namespace UserBundle\Entity;
 
+use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * User
- *
- * @ORM\Table(name="user")
- * @ORM\Entity(repositoryClass="UserBundle\Repository\UserRepository")
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
  */
-class User
+class User extends BaseUser
 {
     /**
-     * @var int
-     *
-     * @ORM\Column(name="id", type="integer")
      * @ORM\Id
+     * @ORM\Column(type="integer")
      * @ORM\GeneratedValue(strategy="AUTO")
      */
-    private $id;
+    protected $id;
 
-
-    /**
-     * Get id.
-     *
-     * @return int
-     */
-    public function getId()
+    public function __construct()
     {
-        return $this->id;
+        parent::__construct();
+        // your own logic
     }
 }
+ 
